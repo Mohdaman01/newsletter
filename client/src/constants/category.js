54a408bacc9e4236c96f6ec58d9ac7c7eb138b1d
Mohdaman01 <mohdastar200026@gmail.com>
2023-12-01 14:15:13 +0530

    simplifying file strucutre

---
+++ client/src/constants/category.js
@@ -0,0 +1,36 @@
+const category = [
+    {
+        name: 'All',
+        value: ''
+    },
+    {
+        name: 'General',
+        value: 'general'
+    },
+    {
+        name: 'Business',
+        value: 'business'
+    },
+    {
+        name: 'Entertainment',
+        value: 'entertainment'
+    },
+    {
+        name: 'Health',
+        value: 'health'
+    },
+    {
+        name: 'Science',
+        value: 'science'
+    },
+    {
+        name: 'Sports',
+        value: 'sports'
+    },
+    {
+        name: 'Technology',
+        value: 'technology'
+    }
+]
+
+export default category;
---
+++ client/src/constants/country.js
@@ -0,0 +1,171 @@
+const countries = [{
+    name: 'All',
+    value: ''
+  },
+  {
+    name: 'India',
+    value: 'in'
+  },
+  {
+    name: 'USA',
+    value: 'us'
+  },
+  {
+    name: 'United Kingdom',
+    value: 'gb',
+  },
+  {
+    name: 'Japan',
+    value: 'jp'
+  },
+  {
+    name: 'France',
+    value: 'fr'
+  },
+  {
+    name: 'Australia',
+    value: 'au'
+  },
+  {
+    name: 'South Korea',
+    value: 'kr'
+  },
+  {
+    name: 'Brazil',
+    value: 'br'
+  },
+  {
+    name: 'Austria',
+    value: 'at'
+  },
+  {
+    name: 'Argentina',
+    value: 'ar'
+  },
+  {
+    name: 'United Arab Emirates',
+    value: 'ae'
+  },
+  {
+    name: 'Canada',
+    value: 'ca'
+  },
+  {
+    name: 'SwitzerLand',
+    value: 'ch'
+  },
+  {
+    name: 'China',
+    value: 'cn'
+  },
+  {
+    name: 'Colombia',
+    value: 'co'
+  },
+  {
+    name: 'Germany',
+    value: 'de'
+  },
+  {
+    name: 'Egypt',
+    value: 'eg'
+  },
+  {
+    name: 'France',
+    value: 'fr'
+  },
+  {
+    name: 'Greece',
+    value: 'gr'
+  },
+  {
+    name: 'Honk Kong',
+    value: 'hk'
+  },
+  {
+    name: 'Indonesia',
+    value: 'id'
+  },
+  {
+    name: 'Ireland',
+    value: 'ir'
+  },
+  {
+    name: 'Israel',
+    value: 'il'
+  },
+  {
+    name: 'Italy',
+    value: 'it'
+  },
+  {
+    name: 'Mexico',
+    value: 'mx'
+  },
+  {
+    name: 'Malaysia',
+    value: 'my'
+  },
+  {
+    name: 'Nigeria',
+    value: 'ng'
+  },
+  {
+    name: 'Netherlands',
+    value: 'nl'
+  },
+  {
+    name: 'Norway',
+    value: 'no'
+  },
+  {
+    name: 'NewZealand',
+    value: 'nz'
+  },
+  {
+    name: 'Philippines',
+    value: 'ph'
+  },
+  {
+    name: 'Poland',
+    value: 'pl'
+  },
+  {
+    name: 'Portugal',
+    value: 'pt'
+  },
+  {
+    name: 'Russia',
+    value: 'ru'
+  },
+  {
+    name: 'Saudi Arabia',
+    value: 'sa'
+  },
+  {
+    name: 'Sweden',
+    value: 'se'
+  },
+  {
+    name: 'Singapore',
+    value: 'sg'
+  },
+  {
+    name: 'Turkey',
+    value: 'tr'
+  },
+  {
+    name: 'Taiwan',
+    value: 'tw'
+  },
+  {
+    name: 'Ukraine',
+    value: 'ua'
+  },
+  {
+    name: 'South Africa',
+    value: 'za'
+  }
+  ];
+
+  export default countries;
